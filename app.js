78a617ca9b3311dc8b97b8a4758e60013ab52da1
sinazo <banganisinazo@gmail.com>
2016-07-28 16:29:48 +0200

    i created route for each week

--- app.js
+++ app.js
@@ -7,43 +7,66 @@ var profitable = require('./profitable');
 var express = require('express');
 var app = express();
 
-var week = process.argv[2]
-var filePath = './data/' + week + '.csv'
-var productList = nelisaProducts.linesInFiles(filePath);
-var productTotal = nelisaProducts.groupingData(productList);
-var getData = nelisaProducts.mostPopular(productTotal);
-var leastData = nelisaProducts.leastPopular(productTotal);
-var mostCategory = nelisaProducts.category(productTotal);
-var productCategory = nelisaProducts.groupingData(productList);
-var leastCategory = nelisaProducts.least(productCategory);
+var getWeeklySales = function(week){
+  //var week = process.argv[2]
+  var filePath = './data/' + week + '.csv'
+  var productList = nelisaProducts.linesInFiles(filePath);
+  var productTotal = nelisaProducts.groupingData(productList);
+  var getData = nelisaProducts.mostPopular(productTotal);
+  var leastData = nelisaProducts.leastPopular(productTotal);
+  var mostCategory = nelisaProducts.category(productTotal);
+  var productCategory = nelisaProducts.groupingData(productList);
+  var leastCategory = nelisaProducts.least(productCategory);
 
-var newPurchase = nelisaPurchases.sales('./data/purchases.csv');
-var purchasesMap = nelisaPurchases.splittingPurchases(newPurchase);
-var weekPurchases = nelisaPurchases.groupPurchasing(purchasesMap,week);
-var purchases = nelisaPurchases.qtySoldCost(weekPurchases,productTotal);
-var salesWeek = nelisaPurchases.nelisaSold(productList);
-var profitability = profitable.findingProfit(salesWeek,purchases);
-var getProfit = profitable.mostProfit(profitability);
-var getCat = mostCat.findingCategories(weekPurchases);
+  var newPurchase = nelisaPurchases.sales('./data/purchases.csv');
+  var purchasesMap = nelisaPurchases.splittingPurchases(newPurchase);
+  var weekPurchases = nelisaPurchases.groupPurchasing(purchasesMap,week);
+  var purchases = nelisaPurchases.qtySoldCost(weekPurchases,productTotal);
+  var salesWeek = nelisaPurchases.nelisaSold(productList);
+  var profitability = profitable.findingProfit(salesWeek,purchases);
+  var getProfit = profitable.mostProfit(profitability);
+  var getCat = mostCat.findingCategories(weekPurchases);
 
-var dataWeek = {
-  stats:[getData,leastData,mostCategory,leastCategory,getProfit,getCat]
-}
-var source = fs.readFileSync('./nelisa.handlebars','utf-8');
-//create template
-var template = handlebars.compile(source);
-//combine the template + data
-var result = template(dataWeek);
-fs.writeFileSync(week +'_weekSales.html',result)
+  var dataWeek = {
+    stats:[getData,leastData,mostCategory,leastCategory,getProfit,getCat]
+  }
+  return dataWeek;
+};
+//
+var createWeeklyData = function(weeklyData){
+  var source = fs.readFileSync('./nelisa.handlebars','utf-8');
+  //create template
+  var template = handlebars.compile(source);
+  //combine the template + data
+  var result = template(weeklyData);
+  return result
+  //fs.writeFileSync(week +'_weekSales.html',result)
+};
 
 // create a route
-app.get('/', function (req, res) {
- res.send('week');
+app.get('/sales/week1', function (req, res) {
+  var sales = getWeeklySales('week1');
+  res.send(createWeeklyData(sales));
 });
+app.get('/sales/week2',function (req, res) {
+  var sales = getWeeklySales('week2');
+  res.send(createWeeklyData(sales));
+});
+app.get('/sales/week3', function (req, res) {
+  var sales = getWeeklySales('week3');
+  res.send(createWeeklyData(sales));
+});
+app.get('/sales/week4', function(req, res) {
+  var sales = getWeeklySales('week4');
+  res.send(createWeeklyData(sales))
+})
+// app.get('/sales/:week_name', function(req, res){
+//   if (req.params.week_name === "week1")
+//         res.send(createWeeklyData('week1'))
+// });
 //start the server
-var server = app.listen(3000, function () {
+var server = app.listen(3005, function () {
 
  var host = server.address().address;
  var port = server.address().port;
-
 });
