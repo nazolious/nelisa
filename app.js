7f4caa6a8f6f9bf6e30a2f296a12fa46ce16a460
sinazo <banganisinazo@gmail.com>
2016-08-02 10:44:43 +0200

    latest

--- app.js
+++ app.js
@@ -6,37 +6,39 @@ var mostCat = require('./mostCat');
 var profitable = require('./profitable');
 var express = require('express');
 var app = express();
+var exphbs  = require('express-handlebars');
 
-var getWeeklySales = function(week){
-  //var week = process.argv[2]
-  var filePath = './data/' + week + '.csv'
-  var productList = nelisaProducts.linesInFiles(filePath);
-  var productTotal = nelisaProducts.groupingData(productList);
-  var getData = nelisaProducts.mostPopular(productTotal);
-  var leastData = nelisaProducts.leastPopular(productTotal);
-  var mostCategory = nelisaProducts.category(productTotal);
-  var productCategory = nelisaProducts.groupingData(productList);
-  var leastCategory = nelisaProducts.least(productCategory);
+var getWeeklySales = function(week) {
+    //var week = process.argv[2]
+    var filePath = './data/' + week + '.csv'
+    var productList = nelisaProducts.linesInFiles(filePath);
+    var productTotal = nelisaProducts.groupingData(productList);
+    var getData = nelisaProducts.mostPopular(productTotal);
+    var leastData = nelisaProducts.leastPopular(productTotal);
+    var mostCategory = nelisaProducts.category(productTotal);
+    var productCategory = nelisaProducts.groupingData(productList);
+    var leastCategory = nelisaProducts.least(productCategory);
 
-  var newPurchase = nelisaPurchases.sales('./data/purchases.csv');
-  var purchasesMap = nelisaPurchases.splittingPurchases(newPurchase);
-  var weekPurchases = nelisaPurchases.groupPurchasing(purchasesMap,week);
-  var purchases = nelisaPurchases.qtySoldCost(weekPurchases,productTotal);
-  var salesWeek = nelisaPurchases.nelisaSold(productList);
-  var profitability = profitable.findingProfit(salesWeek,purchases);
-  var getProfit = profitable.mostProfit(profitability);
-  var getCat = mostCat.findingCategories(weekPurchases);
+    var newPurchase = nelisaPurchases.sales('./data/purchases.csv');
+    var purchasesMap = nelisaPurchases.splittingPurchases(newPurchase);
+    var weekPurchases = nelisaPurchases.groupPurchasing(purchasesMap, week);
+    var purchases = nelisaPurchases.qtySoldCost(weekPurchases, productTotal);
+    var salesWeek = nelisaPurchases.nelisaSold(productList);
+    var profitability = profitable.findingProfit(salesWeek, purchases);
+    var getProfit = profitable.mostProfit(profitability);
+    var getCat = mostCat.findingCategories(weekPurchases);
 
-  var dataWeek = {
-    stats:[getData,leastData,mostCategory,leastCategory,getProfit,getCat]
-  }
-  var source = fs.readFileSync('./nelisa.handlebars','utf-8');
-  //create template
-  var template = handlebars.compile(source);
-  //combine the template + data
-  var result = template(dataWeek);
-  return result
-  //fs.writeFileSync(week +'_weekSales.html',result)
+    var dataWeek = {
+        stats: [getData, leastData, mostCategory, leastCategory, getProfit, getCat]
+    }
+    var source = fs.readFileSync('./views/layouts/main.handlebars', 'utf-8');
+    //create template
+    var template = handlebars.compile(source);
+    //combine the template + data
+    var result = template(dataWeek);
+      // fs.writeFileSync(week +'_weekSales.html',result)
+    return result
+
 };
 
 // create a route
@@ -56,15 +58,13 @@ var getWeeklySales = function(week){
 //   var sales = getWeeklySales('week4');
 //   res.send(createWeeklyData(sales))
 // })
-app.get('/sales/:week_name', function(req, res){
-  // if (req.params.week_name === "week1")
-  var week = req.params.week_name;
-  var sales = getWeeklySales(week);
-        res.send(sales);
+app.get('/sales/:week_name', function(req, res) {
+    var week = req.params.week_name;
+    var sales = getWeeklySales(week);
+    res.send(sales);
 });
 //start the server
-var server = app.listen(3005, function () {
-
- var host = server.address().address;
- var port = server.address().port;
+var server = app.listen(3005, function() {
+    var host = server.address().address;
+    var port = server.address().port;
 });
