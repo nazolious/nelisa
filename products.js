124f1af27a11337bb712102c0411e0bfa34399c8
sinazo <banganisinazo@gmail.com>
2016-09-16 16:13:08 +0200

    not finish product table

--- products.js
+++ products.js
@@ -1,4 +1,5 @@
 var mysql = require('mysql');
+var fs = require('fs');
 var conn = mysql.createConnection({
   host    : 'localhost',
  user     : 'root',
@@ -7,21 +8,81 @@ var conn = mysql.createConnection({
 
 });
 
-var sql = "INSERT INTO products (product) VALUES ?";
 
-//create a list of lists
-var values = [
-    ['milk'],
-    ['bread'],
-    // ['Canned'],
-    // ['Drinks'],
-    // ['Fruit'],
-    // ['Personal Care'],
-    // ['Sugar-Candy'],
-    ['Imasi']
+var categoryIdMap = [];
 
-];
-conn.query(sql, [values], function(err) {
-    if (err) throw err;
-    conn.end();
+conn.query('select * from categories', function(err, results) {
+if (err) throw err;
+
+    var myCategories = {
+        'Milk 1l': 'Dairy',
+        'Imasi': 'Dairy',
+        'Bread': 'Grains',
+        'Chakalaka Can': 'Canned',
+        'Gold Dish Vegetable Curry Can': 'canned',
+        'Fanta 500ml': 'Drinks',
+        'Coke 500ml': 'Drinks',
+        'Cream Soda 500ml': 'Drinks',
+        'Iwisa Pap 5kg': 'Grains',
+        'Top Class Soy Mince': 'Grains',
+        'Shampoo 1 litre': 'Personal Care',
+        'Soap Bar': 'Personal Care',
+        'Bananas - loose': 'Fruit',
+        'Apples - loose': 'Fruit',
+        'Mixed Sweets 5s': 'Sugar-Candy',
+        'Heart Chocolates': 'Sugar-Candy',
+        'Rose (plastic)': 'Gifts and Cards',
+        'Valentine Cards': 'Gifts and Cards'
+    };
+    // build categoryIdMap
+    results.forEach(function(item){
+      var result = {
+        category: item.category,
+        category_id: item.id
+      }
+      categoryIdMap.push(result);
+    })
+
+      // console.log(categoryIdMap);
+
+    // create a new 'list for products' to insert into the database;
+
+    //loop through all the products read from CSV file
+  var readingFiles = fs.readFileSync('./data/week2.csv', 'utf-8').split('\n');
+  var input = [];
+  for (var i = 0; i < readingFiles.length -1; i++) {
+    var data = readingFiles[i].split(',');
+  //  name = data[2];
+    // console.log(readingFiles[i]);
+
+    if(input.indexof(data[2]) === -1){
+      input.push(data[2]);
+    }
+
+  }
+var myValues = [];
+  for (var i = 0; i < input.length; i++) {
+    for (var category in categoryIdMap) {
+    var productName = input[i];
+    var categoryName = myCategories[productName];
+    var category_id = categoryIdMap[categoryName];
+      }
+    }
+  myValues.push([
+    productName,
+    category_id
+  ])
+  console.log(myValues);
+  }
+
+      // what is my productName
+
+      // get the category name for my productName
+
+      // get the database category id from the categoryIdMap
+
+      // insert product name and category_id in 'list for products'
+
+    // now bulk insert 'list for products' into the database
+  conn.end();
 });
--- script.sql
+++ script.sql
@@ -1,8 +1,21 @@
+use nelisa;
+
 -- you can drop tables
 drop table if exists categories;
 
 -- you can create tables in the script;
 create table categories(
     id int primary key auto_increment,
-    category name char(100) not null
+    category char(100) not null
 );
+drop table if exists products;
+create table products (
+    id int primary key auto_increment,
+    product char(100) not null,
+    category_id int,
+    foreign key (category_id) references categories(id),
+    CONSTRAINT uc_product UNIQUE (product)
+);
+
+-- you can write a select query to check for data in a table
+-- select * from products;
