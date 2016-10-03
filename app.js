80b6f50ed81da407dcd4915d7ffc7df170bb34c3
sinazo <banganisinazo@gmail.com>
2016-10-03 13:57:21 +0200

    show cat and products

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+'use strict';
 var fs = require('fs');
 var handlebars = require('handlebars');
 var nelisaProducts = require('./nelisaProducts');
@@ -5,60 +6,87 @@ var nelisaPurchases = require('./nelisaPurchases');
 var mostCat = require('./mostCat');
 var profitable = require('./profitable');
 var express = require('express');
+var mysql = require('mysql');
+var myConnection = require('express-myconnection');
+var bodyParser = require('body-parser');
 var app = express();
 var exphbs  = require('express-handlebars');
+var products = require('./routes/products');
+var categories = require('./routes/categories');
 
-
-var getWeeklySales = function(week) {
-    //var week = process.argv[2]
-    var filePath = './data/' + week + '.csv'
-    var productList = nelisaProducts.linesInFiles(filePath);
-    var productTotal = nelisaProducts.groupingData(productList);
-    var getData = nelisaProducts.mostPopular(productTotal);
-    var leastData = nelisaProducts.leastPopular(productTotal);
-    var mostCategory = nelisaProducts.category(productTotal);
-    var productCategory = nelisaProducts.groupingData(productList);
-    var leastCategory = nelisaProducts.least(productCategory);
-
-    var newPurchase = nelisaPurchases.sales('./data/purchases.csv');
-    var purchasesMap = nelisaPurchases.splittingPurchases(newPurchase);
-    var weekPurchases = nelisaPurchases.groupPurchasing(purchasesMap, week);
-    var purchases = nelisaPurchases.qtySoldCost(weekPurchases, productTotal);
-    var salesWeek = nelisaPurchases.nelisaSold(productList);
-    var profitability = profitable.findingProfit(salesWeek, purchases);
-    var getProfit = profitable.mostProfit(profitability);
-    var getCat = mostCat.findingCategories(weekPurchases);
-
-    var dataWeek = {
-        stats: [getData, leastData, mostCategory, leastCategory, getProfit, getCat]
-    }
-    // var source = fs.readFileSync('./views/layouts/main.handlebars', 'utf-8');
-    //create template
-    // var template = handlebars.compile(source);
-    //combine the template + data
-    // var result = template(dataWeek);
-      // fs.writeFileSync(week +'_weekSales.html',result)
-    return dataWeek;
-
+var dbOptions = {
+      host: 'localhost',
+      user: 'root',
+      password: 'Amani39bangani',
+      port: 3306,
+      database: 'nelisa'
 };
+
+// var getWeeklySales = function(week) {
+//     //var week = process.argv[2]
+//     var filePath = './data/' + week + '.csv'
+//     var productList = nelisaProducts.linesInFiles(filePath);
+//     var productTotal = nelisaProducts.groupingData(productList);
+//     var getData = nelisaProducts.mostPopular(productTotal);
+//     var leastData = nelisaProducts.leastPopular(productTotal);
+//     var mostCategory = nelisaProducts.category(productTotal);
+//     var productCategory = nelisaProducts.groupingData(productList);
+//     var leastCategory = nelisaProducts.least(productCategory);
+//
+//     var newPurchase = nelisaPurchases.sales('./data/purchases.csv');
+//     var purchasesMap = nelisaPurchases.splittingPurchases(newPurchase);
+//     var weekPurchases = nelisaPurchases.groupPurchasing(purchasesMap, week);
+//     var purchases = nelisaPurchases.qtySoldCost(weekPurchases, productTotal);
+//     var salesWeek = nelisaPurchases.nelisaSold(productList);
+//     var profitability = profitable.findingProfit(salesWeek, purchases);
+//     var getProfit = profitable.mostProfit(profitability);
+//     var getCat = mostCat.findingCategories(weekPurchases);
+//
+//     var dataWeek = {
+//         stats: [getData, leastData, mostCategory, leastCategory, getProfit, getCat]
+//     }
+//
+//     return dataWeek;
+//
+// };
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// /setup middleware
+app.use(myConnection(mysql, dbOptions, 'single'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
-app.get('/', function (req, res) {
-    res.render('home');
-  });
+function errorHandler(err, req, res, next) {
+  res.status(500);
+  res.render('error', { error: err });
+}
+//setup the handlers
 
-app.get('/sales/:week_name', function(req, res) {
-    var week = req.params.week_name;
-    // var weekFile = './data/' + week + '.csv';
-    var data = getWeeklySales(week);
-    res.render('weeklyStats',data);
-
-    // var weekFile = "../"getWeeklySales(week);
-    // res.send(weekFile);
-});
+app.get('/categories', categories.show);
+// app.get('/categories/add', categories.showAdd);
+// app.get('/categories/edit/:id', categories.get);
+// app.post('/categories/update/:id', categories.update);
+// app.post('/categories/add', categories.add);
+// //this should be a post but this is only an illustration of CRUD - not on good practices
+// app.get('/categories/delete/:id', categories.delete);
+app.get('/products', products.show);
+// app.get('/', function (req, res) {
+//     res.render('home');
+//   });
+//
+// app.get('/sales/:week_name', function(req, res) {
+//     var week = req.params.week_name;
+//     // var weekFile = './data/' + week + '.csv';
+//     var data = getWeeklySales(week);
+//     res.render('weeklyStats',data);
+//
+//     var weekFile = getWeeklySales(week);
+//     res.send(weekFile);
+// });
 
 app.set('port', (process.env.PORT || 5000));
 //start the app like this:
